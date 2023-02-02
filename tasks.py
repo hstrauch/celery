ec853aa09067b05a3d91298ec57e50f5c1982abf
Hellmuth Strauch <hellmuthstrauch@Hellmuths-MBP.attlocal.net>
2023-02-01 19:03:17 -0500

    Cambio en la funcion add del worker

--- tasks.py
+++ tasks.py
@@ -1,6 +1,7 @@
 import os
 from celery import Celery
 from celery.utils.log import get_task_logger
+import time
 
 app = Celery('tasks', broker=os.getenv("CELERY_BROKER_URL"))
 logger = get_task_logger(__name__)
@@ -8,5 +9,10 @@ logger = get_task_logger(__name__)
 
 @app.task
 def add(x, y):
-    logger.info(f'Adding {x} + {y}')
+    ttimes = 1
+    while ttimes < 100:
+        logger.info(f'Showed {ttimes} times')
+        ttimes + 1
+        time.sleep(10)
+    #logger.info(f'Adding {x} + {y}')
     return x + y
